385bbdf37fd2d8f8de54923bfb7d4f8fb31517fe
Da-Jin Chu <dajinchu@gmail.com>
2015-08-09 10:38:04 -0400

    Created Message and User model classes but they are not implemented

---
+++ app/src/main/java/com/ethangraf/blast/Message.java
@@ -0,0 +1,61 @@
+package com.ethangraf.blast;
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBAttribute;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable;
+
+/**
+ * Created by Da-Jin on 8/9/2015.
+ */
+@DynamoDBTable(tableName = "Messages")
+public class Message {
+
+    String groupID;
+    String datePosted;
+    String subject;
+    String body;
+
+
+    @DynamoDBAttribute(attributeName = "body")
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+
+    @DynamoDBAttribute(attributeName = "Subject")
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+
+
+    @DynamoDBHashKey(attributeName = "Group ID")
+    public String getGroupID() {
+        return groupID;
+    }
+
+    public void setGroupID(String groupID) {
+        this.groupID = groupID;
+    }
+
+    @DynamoDBRangeKey(attributeName = "DatePosted")
+    public String getDatePosted() {
+        return datePosted;
+    }
+
+    public void setDatePosted(String datePosted) {
+        this.datePosted = datePosted;
+    }
+
+
+
+}
