8eda12ad5b37fea630fe1a50849cb0526c31deae
eagraf <graf.ethan@gmail.com>
2015-08-08 21:58:18 -0400

    Added sent messages and archive view.

--- app/src/main/java/com/ethangraf/blast/BlastFragmentPagerAdapter.java
+++ app/src/main/java/com/ethangraf/blast/BlastFragmentPagerAdapter.java
@@ -9,17 +9,21 @@ import android.support.v4.app.FragmentPagerAdapter;
  * Created by Ethan on 8/8/2015.
  */
 public class BlastFragmentPagerAdapter extends FragmentPagerAdapter {
-    final int PAGE_COUNT = 1;
-    private String tabTitles[] = new String[] { "Inbox" };
+    final int PAGE_COUNT = 3;
+    private String tabTitles[] = new String[] { "Inbox", "Sent", "Archived" };
     private Context context;
 
     private InboxFragment inboxFragment;
+    private SentFragment sentFragment;
+    private ArchiveFragment archiveFragment;
 
     public BlastFragmentPagerAdapter(FragmentManager fm, Context context) {
         super(fm);
         this.context = context;
 
         inboxFragment = new InboxFragment();
+        sentFragment = new SentFragment();
+        archiveFragment = new ArchiveFragment();
     }
 
     @Override
@@ -31,13 +35,15 @@ public class BlastFragmentPagerAdapter extends FragmentPagerAdapter {
     public Fragment getItem(int position) {
         switch(position) {
             case 0:
-                break;
+                return inboxFragment;
             case 1:
-                break;
+                return sentFragment;
             case 2:
-                break;
+                return archiveFragment;
+            default:
+                return inboxFragment;
         }
-        return inboxFragment;
+
     }
 
     @Override
---
+++ app/src/main/java/com/ethangraf/blast/SentFragment.java
@@ -0,0 +1,41 @@
+package com.ethangraf.blast;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.Arrays;
+
+/**
+ * Created by Ethan on 8/8/2015.
+ */
+public class SentFragment extends Fragment {
+
+    public RecyclerView mSentView;
+    public InboxAdapter mSentAdapter;
+    public RecyclerView.LayoutManager mSentLayoutManager;
+
+    private static final String planets[] = new String[] {"Sun", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"};
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_sent, container, false);
+
+        mSentView = (RecyclerView) view.findViewById(R.id.sent_list_view);
+        mSentLayoutManager = new LinearLayoutManager(getActivity());
+        mSentView.setLayoutManager(mSentLayoutManager);
+
+        mSentAdapter = new InboxAdapter(Arrays.asList(planets));
+        mSentView.setAdapter(mSentAdapter);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mSentView.setHasFixedSize(true);
+        return view;
+    }
+}
